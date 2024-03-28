806ffb1a302634cbcc96904017c2b9f3fe12bb1d
Artyom <volkovartik@gmail.com>
2024-03-28 14:00:24 +0300

    update code and conf

--- PayPal.py
+++ PayPal.py
@@ -4,13 +4,15 @@
 1/2 документ плагина
 """
 import logging
-import os
 import time
-
 import dateparser
+from datetime import datetime
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.chrome.webdriver import WebDriver
 from src.spp.types import SPP_document
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 class PayPal:
     """
@@ -28,7 +30,8 @@ class PayPal:
     HOST = "https://newsroom.paypal-corp.com/news"
     _content_document: list[SPP_document]
 
-    def __init__(self, driver, *args, **kwargs):
+    def __init__(self, webdriver: WebDriver, last_document: SPP_document = None, max_count_documents: int = 100,
+                 *args, **kwargs):
         """
         Конструктор класса парсера
 
@@ -37,18 +40,14 @@ class PayPal:
         """
         # Обнуление списка
         self._content_document = []
-        self.driver = driver
+
+        self.driver = webdriver
+        self.wait = WebDriverWait(self.driver, timeout=20)
+        self.max_count_documents = max_count_documents
+        self.last_document = last_document
 
         # Логер должен подключаться так. Вся настройка лежит на платформе
         self.logger = logging.getLogger(self.__class__.__name__)
-
-        # УДалить DRAFT
-        logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
-        consoleHandler = logging.StreamHandler()
-        consoleHandler.setFormatter(logFormatter)
-        self.logger.addHandler(consoleHandler)
-        #
-
         self.logger.debug(f"Parser class init completed")
         self.logger.info(f"Set source: {self.SOURCE_NAME}")
         ...
@@ -60,8 +59,12 @@ class PayPal:
         :rtype:
         """
         self.logger.debug("Parse process start")
-        self._parse()
-        self.logger.debug("Parse process finished")
+        try:
+            self._parse()
+        except Exception as e:
+            self.logger.debug(f'Parsing stopped with error: {e}')
+        else:
+            self.logger.debug("Parse process finished")
         return self._content_document
 
     def _parse(self):
@@ -77,29 +80,28 @@ class PayPal:
         # Тут должен находится блок кода, отвечающий за парсинг конкретного источника
         # -
 
-        self.driver.get(
-            "https://newsroom.paypal-corp.com/news")  # Открыть первую страницу с материалами EMVCo в браузере
+        self.driver.get(self.HOST)  # Открыть первую страницу с материалами
         time.sleep(5)
 
         # Окно с куками пропадает самостоятельно через 2-3 секунды
         try:
             cookies_btn = self.driver.find_element(By.ID, 'acceptAllButton').find_element(By.XPATH,
-                                                                                            '//*[@id="acceptAllButton"]')
+                                                                                          '//*[@id="acceptAllButton"]')
             self.driver.execute_script('arguments[0].click()', cookies_btn)
-            self.logger.info('Cookies убран')
+            self.logger.debug('Cookies убран')
         except:
             self.logger.exception('Не найден cookies')
             pass
 
-        #self.logger.info('Прекращен поиск Cookies')
-        #time.sleep(3)
+        # self.logger.info('Прекращен поиск Cookies')
+        # time.sleep(3)
         counter = 0
         flag = True
         while flag:
 
             self.logger.debug('Загрузка списка элементов...')
             doc_table = self.driver.find_element(By.CLASS_NAME, 'wd_item_list').find_elements(By.CLASS_NAME,
-                                                                                                   'wd_has-image')
+                                                                                              'wd_has-image')
             self.logger.debug('Обработка списка элементов...')
 
             # Цикл по всем строкам таблицы элементов на текущей странице
@@ -109,31 +111,30 @@ class PayPal:
 
                 try:
                     title = element.find_element(By.CLASS_NAME, 'wd_title').text
-                    #title = element.find_element(By.XPATH, '//*[@id="feed-item-title-1"]/a').text
+                    # title = element.find_element(By.XPATH, '//*[@id="feed-item-title-1"]/a').text
 
                 except:
                     self.logger.exception('Не удалось извлечь title')
                     title = ' '
 
-
-                #try:
+                # try:
                 #    other_data = element.find_elements(By.CLASS_NAME, "wd_category_link_list").text
-                #except:
+                # except:
                 #    self.logger.exception('Не удалось извлечь other_data')
                 #    other_data = ''
-                #// *[ @ id = "main-content"] / ul / li[1] / div[2] / span[2]
-                #// *[ @ id = "main-content"] / ul / li[2] / div[2] / span[2]
-
+                # // *[ @ id = "main-content"] / ul / li[1] / div[2] / span[2]
+                # // *[ @ id = "main-content"] / ul / li[2] / div[2] / span[2]
 
                 try:
                     date = dateparser.parse(element.find_element(By.CLASS_NAME, 'wd_date').text)
                 except:
                     self.logger.exception('Не удалось извлечь date_text')
-                    date = ' '
+                    date = None
+                    continue
 
-                #try:
+                # try:
                 #    date = dateparser.parse(date_text)
-                #except:
+                # except:
                 #    self.logger.exception('Не удалось извлечь date')
                 #    date = None
 
@@ -141,58 +142,67 @@ class PayPal:
                     abstract = element.find_element(By.CLASS_NAME, 'wd_summary').text
                 except:
                     self.logger.exception('Не удалось извлечь abstract')
-                    abstract = ' '
-
-                book = ' '
+                    abstract = ''
 
                 try:
                     web_link = element.find_element(By.TAG_NAME, 'a').get_attribute('href')
                 except:
                     self.logger.exception('Не удалось извлечь web_link')
                     web_link = None
+                    continue
 
-                self._content_document.append(SPP_document(
+                self.driver.execute_script("window.open('');")
+                self.driver.switch_to.window(self.driver.window_handles[1])
+                self.driver.get(web_link)
+                self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.wd_news_body')))
+
+                text_content = self.driver.find_element(By.CLASS_NAME, 'wd_news_body').text
+
+                cats = [x.text for x in self.driver.find_elements(By.CLASS_NAME, 'wd_category_link')]
+
+                other_data = {'categories': cats}
+
+                doc = SPP_document(
                     doc_id=None,
                     title=title,
                     abstract=abstract,
-                    text=None,
+                    text=text_content,
                     web_link=web_link,
                     local_link=None,
-                    other_data=None,
+                    other_data=other_data,
                     pub_date=date,
-                    load_date=None,
-                ))
-                counter += 1
-                if counter > 15:
-                    flag = False
+                    load_date=datetime.now(),
+                )
 
+                self.find_document(doc)
 
+                self.driver.close()
+                self.driver.switch_to.window(self.driver.window_handles[0])
 
             try:
-                pagination_arrow = self.driver.find_element(By.XPATH, '//*[@id="wd_printable_content"]/div/div[4]/nav/ul/li[13]/a')
-                #self.driver.execute_script('arguments[0].click()', pagination_arrow)
-                pagination_arrow.click()
+                pagination_arrow = self.driver.find_element(By.XPATH, '//li[contains(@class, \'wd_page_next\')]')
+                self.driver.execute_script('arguments[0].click()', pagination_arrow)
+                # pagination_arrow.click()
                 time.sleep(3)
-                #pg_num = self.driver.find_element(By.ID, 'current_page').text
-                self.logger.info(f'Выполнен переход на след. страницу')
+                # pg_num = self.driver.find_element(By.ID, 'current_page').text
+                self.logger.debug(f'Выполнен переход на след. страницу')
 
-                #if int(pg_num) > 3:
+                # if int(pg_num) > 3:
                 #    self.logger.info('Выполнен переход на 4-ую страницу. Принудительное завершение парсинга.')
-                #break
+                # break
 
             except:
                 self.logger.exception('Не удалось найти переход на след. страницу. Прерывание цикла обработки')
                 break
 
         # Логирование найденного документа
-            #self.logger.info(self._find_document_text_for_logger(_content_document))
+        # self.logger.info(self._find_document_text_for_logger(_content_document))
 
         # ---
         # ========================================
         ...
 
-    @staticmethod
-    def _find_document_text_for_logger(doc: SPP_document):
+    def _find_document_text_for_logger(self, doc: SPP_document):
         """
         Единый для всех парсеров метод, который подготовит на основе SPP_document строку для логера
         :param doc: Документ, полученный парсером во время своей работы
@@ -202,52 +212,15 @@ class PayPal:
         """
         return f"Find document | name: {doc.title} | link to web: {doc.web_link} | publication date: {doc.pub_date}"
 
-    @staticmethod
-    def some_necessary_method():
+    def find_document(self, _doc: SPP_document):
         """
-        Если для парсинга нужен какой-то метод, то его нужно писать в классе.
-
-        Например: конвертация дат и времени, конвертация версий документов и т. д.
-        :return:
-        :rtype:
+        Метод для обработки найденного документа источника
         """
-        ...
+        if self.last_document and self.last_document.hash == _doc.hash:
+            raise Exception(f"Find already existing document ({self.last_document})")
 
-    @staticmethod
-    def nasty_download(driver, path: str, url: str) -> str:
-        """
-        Метод для "противных" источников. Для разных источника он может отличаться.
-        Но основной его задачей является:
-            доведение driver селениума до файла непосредственно.
+        if self.max_count_documents and len(self._content_document) >= self.max_count_documents:
+            raise Exception(f"Max count articles reached ({self.max_count_documents})")
 
-            Например: пройти куки, ввод форм и т. п.
-
-        Метод скачивает документ по пути, указанному в driver, и возвращает имя файла, который был сохранен
-        :param driver: WebInstallDriver, должен быть с настроенным местом скачивания
-        :_type driver: WebInstallDriver
-        :param url:
-        :_type url:
-        :return:
-        :rtype:
-        """
-
-        with driver:
-            driver.set_page_load_timeout(40)
-            driver.get(url=url)
-            time.sleep(1)
-
-            # ========================================
-            # Тут должен находится блок кода, отвечающий за конкретный источник
-            # -
-            # ---
-            # ========================================
-
-            # Ожидание полной загрузки файла
-            while not os.path.exists(path + '/' + url.split('/')[-1]):
-                time.sleep(1)
-
-            if os.path.isfile(path + '/' + url.split('/')[-1]):
-                # filename
-                return url.split('/')[-1]
-            else:
-                return ""
+        self._content_document.append(_doc)
+        self.logger.info(self._find_document_text_for_logger(_doc))
--- main.py
+++ main.py
@@ -1,13 +1,56 @@
 from selenium import webdriver
-
 from logging import config
-config.fileConfig('dev.logger.conf')
 from PayPal import PayPal
+import pickle
+import pandas
 
-driver = webdriver.Chrome()
+from src.spp.types import SPP_document
 
-parser = PayPal(driver)
-docs = parser.content()
+config.fileConfig('dev.logger.conf')
 
+
+def driver():
+    """
+    Selenium web driver
+    """
+    options = webdriver.ChromeOptions()
+
+    # Параметр для того, чтобы браузер не открывался.
+    # options.add_argument('headless')
+
+    options.add_argument('window-size=1920x1080')
+    options.add_argument("disable-gpu")
+
+    return webdriver.Chrome(options)
+
+
+def to_dict(doc: SPP_document) -> dict:
+    return {
+        'title': doc.title,
+        'abstract': doc.abstract,
+        'text': doc.text,
+        'web_link': doc.web_link,
+        'local_link': doc.local_link,
+        'other_data': doc.other_data.get('category') if doc.other_data.get('category') else '',
+        'pub_date': str(doc.pub_date.timestamp()) if doc.pub_date else '',
+        'load_date': str(doc.load_date.timestamp()) if doc.load_date else '',
+    }
+
+
+parser = PayPal(driver(), max_count_documents=50)
+docs: list[SPP_document] = parser.content()
+
+try:
+    with open('backup/documents.backup.pkl', 'wb') as file:
+        pickle.dump(docs, file)
+except Exception as e:
+    print(e)
+
+try:
+    dataframe = pandas.DataFrame.from_records([to_dict(d) for d in docs])
+    dataframe.to_csv('out/paypal_documents.csv')
+except Exception as e:
+    print(e)
 
 print(*docs, sep='\n\r\n')
+print(len(docs))
